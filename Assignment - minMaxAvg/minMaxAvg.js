3f97338766a30c6565da1f96de3c5f0152f8784a
saktish30 <saktishmano@gmail.com>
2021-10-04 22:07:02 +0800

    commit

---
+++ Assignment - minMaxAvg/minMaxAvg.js
@@ -0,0 +1,15 @@
+function maxMinAvg(arr) {
+    var max = arr[0];
+    var min = arr[0];
+    var sum = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        sum = sum + arr[i];
+    }
+    return [max, min, sum/arr.length]; 
+}
